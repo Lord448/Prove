19106218eca3ea9597ba4773eca666f820ffad8f
Lord448 <pedroeroca@outlook.com>
2022-04-22 20:00:09 -0500

    Segundo commit

--- prueba.java
+++ prueba.java
@@ -1,8 +1,14 @@
+import java.util.Scanner;
+
 class prueba {
+
+    int prueba = 1;
+
     public static void main(String[] args) {
         System.out.println("Amo a mi novia Andrea");
         System.out.println("También te amo");
         System.out.println("También te amo");
+        Scanner sc = new Scanner(System.in);
 
     }
 }
