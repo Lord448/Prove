5583784dfc99c94ebfb2a3faee7cbdeb9c1bbb52
Coffee448 <andy_may_hazi_mejoresamigas@hotmail.com>
2022-04-22 19:48:36 -0500

    añadí te amo

--- prueba.java
+++ prueba.java
@@ -1,5 +1,8 @@
 class prueba {
     public static void main(String[] args) {
         System.out.println("Amo a mi novia Andrea");
+        System.out.println("También te amo");
+        System.out.println("También te amo");
+
     }
 }
